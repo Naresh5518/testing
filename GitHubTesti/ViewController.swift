6a7a5c1c1a24713b6686419f78f5a17d585888e1
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-15 18:38:17 +0530

    Initial Commit

---
+++ GitHubTesti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTesti
+//
+//  Created by mTL NARESH on 15/10/18.
+//  Copyright © 2018 mTL NARESH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
